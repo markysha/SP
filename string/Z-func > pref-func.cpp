320de1a6364c10572d62a60138e6fe238f489f5f
markysha <krnejchik@mail.ru>
2016-03-13 10:02:31 +0000

    Update Z-func > pref-func.cpp

--- string/Z-func > pref-func.cpp
+++ string/Z-func > pref-func.cpp
@@ -1,8 +1,8 @@
 vector < int > Z, P;
 
 for(int i = 1; i < n; i++)
-	if(Z[i] > 0)
-		for(int j = Z[i] - 1; j >= 0 && !(P[i + j]); j--)
-			P[i + j] = j + 1;
+  if(Z[i] > 0)
+    for(int j = Z[i] - 1; j >= 0 && !(P[i + j]); j--)
+      P[i + j] = j + 1;
 
 ///taken from http://codeforces.com/blog/entry/9612
