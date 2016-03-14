8fe0baba19c87e8101dc0abb5991963e7eff5a6f
markysha <krnejchik@mail.ru>
2016-03-14 19:10:16 +0000

    Create sumtree_1.cpp

---
+++ fenwick_trees/sumtree_1.cpp
@@ -0,0 +1,26 @@
+struct fenwicktree_1{
+  vector < int > t;
+  int n;
+
+	fenwicktree(int x){
+		n = x;
+		t.resize(n, 0);
+	}
+
+  int sum (int r) {
+    int ans = 0;
+    for (; r >= 0; r = (r & (r + 1)) - 1)
+      ans += t[r];
+    return ans;
+  }
+
+  void inc (int i, int delta) {
+    for (; i < n; i = (i | (i + 1)))
+      t[i] += delta;
+  }
+
+  int sum (int l, int r)
+  {
+    return sum (r) - sum (l - 1);
+  }
+}
