6ca7e77c9f3b0bc307e18eed04018e09488a1ae8
markysha <krnejchik@mail.ru>
2016-07-28 00:18:07 +0430

    Update fibonacci_number.cpp

--- math/fibonacci_number.cpp
+++ math/fibonacci_number.cpp
@@ -2,9 +2,9 @@ unordered_map<long long, long long> Fib;
 
 long long fib(long long n){
     if (n < 2) 
-      return 1;
+        return 1;
     if (Fib.find(n) != Fib.end()) 
-      return Fib[n];
+        return Fib[n];
     Fib[n] = (fib((n + 1) / 2) * fib(n / 2) + fib((n - 1) / 2) * fib((n - 2) / 2)) % MOD;
     return Fib[n];
 }
