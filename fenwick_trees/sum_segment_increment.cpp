e76f41e90b4827a299d4b06450ec67739754fffe
markysha <krnejchik@mail.ru>
2017-05-30 18:46:08 +0300

    Update sum_segment_increment.cpp

--- fenwick_trees/sum_segment_increment.cpp
+++ fenwick_trees/sum_segment_increment.cpp
@@ -2,17 +2,17 @@ template<class T>
 struct fenwick {
     int n;
     vector<T> m, mt;
-    fenwick(int n):n(n), m(n,0), mt(n,0) {}
+    fenwick(int n):n(n), m(n, 0), mt(n, 0) {}
     void add_range(int r, T d) {
         if(r < 0) return;
-        for (int i = r; i >= 0; i = (i&(i+1)) - 1)
+        for (int i = r; i >= 0; i = (i&(i + 1)) - 1)
             mt[i] += d;
-        for (int i = r|(r+1); i < n; i |= i + 1)
-            m[i] += d * (r-(i&(i+1))+1);
+        for (int i = r|(r + 1); i < n; i |= i + 1)
+            m[i] += d * (r-(i&(i + 1)) + 1);
     }
     void add_range(int l, int r, T d) {
         add_range(r, d);
-        add_range(l-1, -d);
+        add_range(l - 1, -d);
     }
     T query(int r) {
         if(r < 0) return 0;
@@ -20,7 +20,7 @@ struct fenwick {
         for (int i = r; i >= 0; i = (i&(i + 1)) - 1)
             res += m[i] + mt[i] * (i-(i&(i + 1)) + 1);
         for (int i = r|(r + 1); i < n; i |= i + 1)
-            res += mt[i] * (r-(i&(i + 1)) + 1);
+            res += mt[i] * (r - (i&(i + 1)) + 1);
         return res;
     }
     T query(int l, int r) {
